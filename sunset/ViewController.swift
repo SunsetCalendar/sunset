eb6cd869bf5b39742d6c44d3904c3a8b0b862a31
Homma Takuma <takuma.honma@pepabo.com>
2016-09-08 17:18:58 +0900

    Initial Commit

---
+++ sunset/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sunset
+//
+//  Created by usr0600429 on 2016/09/08.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sunsetTests/sunsetTests.swift
@@ -0,0 +1,36 @@
+//
+//  sunsetTests.swift
+//  sunsetTests
+//
+//  Created by usr0600429 on 2016/09/08.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import XCTest
+@testable import sunset
+
+class sunsetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
