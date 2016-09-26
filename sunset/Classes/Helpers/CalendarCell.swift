0e7e3cd8c9b91bb91311fe83dcff1b1abc3c653e
Homma Takuma <sfprhythnn@gmail.com>
2016-09-26 17:15:59 +0900

    calendarViewControllerを作成

---
+++ sunset/Classes/Helpers/CalendarCell.swift
@@ -0,0 +1,24 @@
+
+// CalendarCell.swift
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+    
+    var textLabel: UILabel!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+        textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+        textLabel.font = UIFont(name: "HiraKakuProN-W3", size: 11.5)
+        textLabel.textAlignment = NSTextAlignment.center
+        
+        // Cellに追加
+        self.addSubview(textLabel!)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+}
---
+++ sunset/Classes/Models/DateManager.swift
@@ -0,0 +1,83 @@
+
+
+import UIKit
+
+extension Date {
+    func monthAgoDate() -> Date {
+        let addValue = -1
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.month = addValue
+        return (calendar as NSCalendar).date(byAdding: dateComponents, to: self, options: NSCalendar.Options(rawValue: 0))!
+    }
+    
+    func monthLaterDate() -> Date {
+        let addValue: Int = 1
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.month = addValue
+        return (calendar as NSCalendar).date(byAdding: dateComponents, to: self, options: NSCalendar.Options(rawValue: 0))!
+    }
+    
+}
+
+class DateManager: NSObject {
+    var currentMonthOfDates = [Date]() //表記する月の配列
+    var selectedDate = Date()
+    let daysPerWeek: Int = 7
+    var numberOfItems: Int!
+    
+    //月ごとのセルの数を返すメソッド
+    func daysAcquisition() -> Int {
+        let rangeOfWeeks = (Calendar.current as NSCalendar).range(of: NSCalendar.Unit.weekOfMonth, in: NSCalendar.Unit.month, for: firstDateOfMonth())
+        let numberOfWeeks = rangeOfWeeks.length //月が持つ週の数
+        numberOfItems = numberOfWeeks * daysPerWeek //週の数×列の数
+        return numberOfItems
+    }
+    //月の初日を取得
+    func firstDateOfMonth() -> Date {
+        var components = (Calendar.current as NSCalendar).components([.year, .month, .day],
+                                                                     from: selectedDate)
+        components.day = 1
+        let firstDateMonth = Calendar.current.date(from: components)!
+        return firstDateMonth
+    }
+    
+    func dateForCellAtIndexPath(_ numberOfItems: Int) {
+
+        // 月の初日が週の何日目か を計算する
+        let ordinalityOfFirstDay = (Calendar.current as NSCalendar).ordinality(of: NSCalendar.Unit.day, in: NSCalendar.Unit.weekOfMonth, for: firstDateOfMonth())
+        
+        for i in 0...numberOfItems {
+            // 月の初日 と indexPath.item番目のセルに表示する日 の差を計算する
+            var dateComponents = DateComponents()
+            dateComponents.day = i - (ordinalityOfFirstDay - 1)
+            //  表示する月の初日から②で計算した差を引いた日付を取得
+            let date = (Calendar.current as NSCalendar).date(byAdding: dateComponents, to: firstDateOfMonth(), options: NSCalendar.Options(rawValue: 0))!
+            // 配列に追加
+            currentMonthOfDates.append(date)
+        }
+    }
+    
+    // 表記の変更
+    func conversionDateFormat(_ indexPath: IndexPath) -> String {
+        dateForCellAtIndexPath(numberOfItems)
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateFormat = "d"
+        return formatter.string(from: currentMonthOfDates[(indexPath as NSIndexPath).row])
+    }
+    
+    func prevMonth(_ date: Date) -> Date {
+        currentMonthOfDates = []
+        selectedDate = date.monthAgoDate()
+        return selectedDate
+    }
+    
+    // 次月の表示
+    func nextMonth(_ date: Date) -> Date {
+        currentMonthOfDates = []
+        selectedDate = date.monthLaterDate()
+        return selectedDate
+    }
+}
+
