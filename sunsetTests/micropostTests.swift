4187a533757712ad0942ff16f8f52ff8cba2f0ef
asuforce <asuforcegt@hotmail.co.jp>
2016-09-28 12:27:14 +0900

    testファイルを追加

---
+++ sunsetTests/micropostTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import sunset
+
+class micropostTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testGetMicroposts() {
+        
+    }
+}
---
+++ sunsetUITests/micropostUITests.swift
@@ -0,0 +1,23 @@
+import XCTest
+
+class micropostUITests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        continueAfterFailure = false
+
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testShowMicroposts() {
+        let app = XCUIApplication()
+        app.buttons["Micropost"].tap()
+        XCTAssertTrue(app.tables.staticTexts["わくわく"].exists)
+    }
+
+}
