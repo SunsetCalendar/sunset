c538d389e911b9d3f9973b18e7ce8dc7631ca308
asuforce <asuforcegt@hotmail.co.jp>
2016-09-29 16:19:19 +0900

    webサイトの表示ロジックを作成

--- sunset/Classes/Controllers/WebViewController.swift
+++ sunset/Classes/Controllers/WebViewController.swift
@@ -1,9 +1,18 @@
 import UIKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet weak var webView: UIWebView!
+
+    let initUrl = URL(string: "http://google.com/")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.webView.delegate = self
+
+        let request = URLRequest(url: initUrl!)
+        self.webView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
