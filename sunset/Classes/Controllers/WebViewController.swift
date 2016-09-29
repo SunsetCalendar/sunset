d3d4fef322f507ec3edbcb171fa88114d4413249
asuforce <asuforcegt@hotmail.co.jp>
2016-09-29 14:14:47 +0900

    WebViewControllerを追加し関連付け

---
+++ sunset/Classes/Controllers/WebViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
