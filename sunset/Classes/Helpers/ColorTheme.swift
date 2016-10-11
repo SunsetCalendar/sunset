359c45596b6350149a8d62cc80137eb24ae0ca2c
asuforce <asuforcegt@hotmail.co.jp>
2016-10-11 18:05:58 +0900

    UIColorの独自定義を切り出して共通化

---
+++ sunset/Classes/Helpers/ColorTheme.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+extension UIColor {
+    class func lightBlue() -> UIColor {
+        return UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
+    }
+
+    class func lightRed() -> UIColor {
+        return UIColor(red: 195.0 / 255, green: 123.0 / 255, blue: 175.0 / 255, alpha: 1.0)
+    }
+
+    class func darkIndigo() -> UIColor {
+        return UIColor(red:0.07, green:0.13, blue:0.26, alpha:1)
+    }
+
+    class func lightIndigo() -> UIColor {
+        return UIColor(red:0.54, green:0.74, blue:0.74, alpha:1)
+    }
+}
