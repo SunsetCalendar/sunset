1124274788f790021c0a452dfd4c19213679d77f
Homma Takuma <sfprhythnn@gmail.com>
2016-10-18 21:19:11 +0900

    Tweetモデルを保存していなかったので修正

--- sunset/Classes/Models/Tweet.swift
+++ sunset/Classes/Models/Tweet.swift
@@ -1,9 +1,17 @@
-//
-//  Tweet.swift
-//  sunset
-//
-//  Created by Homma Takuma on 2016/10/18.
-//  Copyright © 2016年 GMO Pepabo. All rights reserved.
-//
-
 import Foundation
+import RealmSwift
+
+class Tweet: Object {
+    dynamic var tweet_id: String = ""
+    dynamic var user_id: String = ""
+    dynamic var content: String = ""
+    dynamic var created_at: String = ""
+
+    override static func primaryKey() -> String? {
+        return "tweet_id"
+    }
+
+    override static func indexedProperties() -> [String] {
+        return ["created_at"]
+    }
+}
