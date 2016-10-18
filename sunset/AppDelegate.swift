e46eed5f31df6a8c9a3613443481248f28c0934f
Homma Takuma <sfprhythnn@gmail.com>
2016-10-18 21:01:50 +0900

    tweet固有のURLを作成したいので、共有する変数を追加

--- sunset/AppDelegate.swift
+++ sunset/AppDelegate.swift
@@ -9,13 +9,14 @@ import Keys
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var micropostId: String?
     var targetDate: String?
     var calendarCellWidth: CGFloat?
     var calendarCellHeight: CGFloat?
     let realm: Realm = try! Realm()
     let sunsetKeys = SunsetKeys()
-
+    var userID: String?
+    var tweetID: String?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
@@ -23,10 +24,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let formatter: DateFormatter = DateFormatter()
             formatter.dateFormat = "yyyy-MM-dd"
 
-            let fetchData: [Post] = realm.objects(Post.self).map{$0}
-            for post in fetchData {
+            let fetchData: [Tweet] = realm.objects(Tweet.self).map{$0}
+            for tweet in fetchData {
                 try! realm.write() {
-                    realm.delete(post)
+                    realm.delete(tweet)
                 }
             }
             
