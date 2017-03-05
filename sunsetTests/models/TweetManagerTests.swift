c646147f955517fbd806515058b224aff7bff3fe
Homma Takuma <nagomimatcha@gmail.com>
2017-03-05 11:37:23 +0900

    ツイートの内容のテスト追加

--- sunsetTests/models/TweetManagerTests.swift
+++ sunsetTests/models/TweetManagerTests.swift
@@ -7,14 +7,14 @@ class TweetManagerTEsts: XCTestCase {
     
     let realm: Realm = try! Realm()
     let tweetManager: TweetManager = TweetManager()
+    let tweet: Tweet = Tweet()
     
     override func setUp() {
         super.setUp()
-        let tweet: Tweet = Tweet()
-        tweet.content = "平民なので申し訳なく鳥貴族に行かせていただきます"
-        tweet.created_at = "2017-01-01"
-        tweet.user_id = "sunset"
-        tweet.tweet_id = "1"
+        self.tweet.content = "平民なので申し訳なく鳥貴族に行かせていただきます"
+        self.tweet.created_at = "2017-01-01"
+        self.tweet.user_id = "sunset"
+        self.tweet.tweet_id = "1"
         try! self.realm.write() {
             self.realm.add(tweet)
         }
@@ -29,8 +29,12 @@ class TweetManagerTEsts: XCTestCase {
     }
     
     func testFilter() {
-        let oneTweet = self.tweetManager.filter(date: "2017-01-01")
+        let oneTweet: [Tweet] = self.tweetManager.filter(date: "2017-01-01")
         XCTAssertEqual(oneTweet.count, 1)
+        XCTAssertEqual(oneTweet[0].content, self.tweet.content)
+        XCTAssertEqual(oneTweet[0].created_at, self.tweet.created_at)
+        XCTAssertEqual(oneTweet[0].user_id, self.tweet.user_id)
+        XCTAssertEqual(oneTweet[0].tweet_id, self.tweet.tweet_id)
         
         let zeroTweets = self.tweetManager.filter(date: "2017-01-02")
         XCTAssertEqual(zeroTweets.count, 0)
